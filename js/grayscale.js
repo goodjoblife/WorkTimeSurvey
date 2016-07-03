827f9cfe495340469af14da5dbe04941b9416511
Wei-Ming <barry800414@gmail.com>
2016-07-03 21:32:52 +0800

    add overtime frequency into form

--- js/grayscale.js
+++ js/grayscale.js
@@ -152,6 +152,7 @@ function checkForm () {
     var company_name = $("#company_name").val();
     var job_title = $("#job_title").val();
     var week_work_time = $("#week_work_time").val();
+    var overtime_frequency = $("#overtime_frequency_input input[name='frequency']:checked").val();
     var email = $("#email").val();
     /*
     var salary_type = $("#salary_type").val();
@@ -180,6 +181,10 @@ function checkForm () {
     }
     if (week_work_time == ''){  
         return "需填平均每週工時";
+    }
+
+    if(overtime_frequency === undefined || overtime_frequency == ''){
+        return "需填寫加班頻率";
     }
     /*
     if (salary_min != '' || salary_max != '') {
@@ -222,6 +227,7 @@ function submitForm() {
             company_name: $("#company_name").val(),
             job_title: $("#job_title").val(),
             week_work_time: $("#week_work_time").val(),
+            overtime_frequency: $("#overtime_frequency_input input[name='frequency']:checked").val(),
             email: $("#email").val(),
             salary_type: $("#salary_type").val(),
             salary_min: $("#salary_min").val(),
