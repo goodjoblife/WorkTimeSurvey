bd814e8e85f78a4a4a3c9312f429851ff39fecde
Qing-Cheng Li <qc.linux@gmail.com>
2016-07-17 23:22:23 +0800

    Tracking navigation events (an example)

---
+++ js/index-i13n.js
@@ -0,0 +1,11 @@
+$(function(){
+    var category = "LANDING_PAGE";
+
+    // Tracking click events
+    $("a").click(function(e){
+        if (this.dataset['logaction'] && ga) {
+            var logAction = this.dataset['logaction'];
+            ga('send', 'event', category, logAction);
+        }
+    });
+});
