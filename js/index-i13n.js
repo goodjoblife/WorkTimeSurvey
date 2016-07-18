45318e019c7a76cc837802043d1d57ae9336660b
Wei-Ming <barry800414@gmail.com>
2016-07-18 20:01:06 +0800

    add ga tracking when focusing on the input of form

--- js/index-i13n.js
+++ js/index-i13n.js
@@ -8,4 +8,11 @@ $(function(){
             ga('send', 'event', category, logAction);
         }
     });
+
+    $("#form input").focus(function(e){
+    	if(this.dataset['logaction'] && ga) {
+            var logAction = this.dataset['logaction'];
+            ga('send', 'event', category, logAction);
+        }
+    });
 });
