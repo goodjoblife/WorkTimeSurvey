865d8222b2473b2a5e8fed9e2ce6a09800d33880
Wei-Ming <barry800414@gmail.com>
2016-07-26 16:14:39 +0800

    remvove console message of general-i13n.js

--- js/general-i13n.js
+++ js/general-i13n.js
@@ -3,7 +3,6 @@ $(function(){
     $("a").click(function(e){
         if (this.dataset['logcategory'] && this.dataset['logaction'] && ga) {
             var category = this.dataset['logcategory'];
-            console.log(category);
             var logAction = this.dataset['logaction'];
             ga('send', 'event', category, logAction);
         }
