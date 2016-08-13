5e1a30763abd8b5c7f353aad35dd6cda355b72e4
Yen-chi Chen <mark86092@gmail.com>
2016-08-13 11:14:10 +0800

    travis ci auto build

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+REV=`git describe --always`
+
+rm -rf public
+git clone git@github.com:goodjoblife/WorkTimeSurvey.git public -b dev-gh-pages --depth 1
+
+# Remove all the old build files
+
+rm -rf public/* public/.gitignore
+cp CNAME public/
+
+# Build the page into public
+
+cp -r bower_components css font-awesome fonts img js *.html public
+
+# Build done, now commit it
+
+cd public
+git add -A .
+git commit -m "regen for $REV"
+#git push origin dev-gh-pages
