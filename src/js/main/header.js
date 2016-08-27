bc87b28b6c0a17171eec4529e97cd588c46daf82
Tin <wutingy@gmail.com>
2016-08-27 11:14:01 +0800

    [fomr] add form style

--- src/js/main/header.js
+++ src/js/main/header.js
@@ -1,7 +1,7 @@
 const header = document.getElementById('header');
 
 if (breakpoint.above_small.matches) {
-  window.addEventListener('scroll', function() {
+  window.addEventListener('scroll', () => {
     if (document.documentElement && document.documentElement.scrollTop || document.body.scrollTop > 100) {
       header.classList.add('is-fixed');
     } else if (document.documentElement && document.documentElement.scrollTop || document.body.scrollTop == 0) {
@@ -12,7 +12,7 @@ if (breakpoint.above_small.matches) {
 
 const header_button = document.getElementById('header-button');
 const header_nav = document.getElementById('header-nav');
-header_button.addEventListener('click', function() {
+header_button.addEventListener('click', () => {
   this.classList.toggle('is-open');
   header_nav.classList.toggle('is-open');
   document.querySelector('.btn-scroll-down').classList.toggle('hide');
@@ -20,7 +20,7 @@ header_button.addEventListener('click', function() {
 
 const nav_scroll = document.querySelectorAll('.js-scrollpage');
 for (let i = 0; i < nav_scroll.length; i++) {
-  nav_scroll[i].addEventListener('click', function() {
+  nav_scroll[i].addEventListener('click', () => {
     header_button.classList.remove('is-open');
     header_nav.classList.remove('is-open');
   });
--- src/js/main/index.js
+++ src/js/main/index.js
@@ -1,18 +1,19 @@
 const scroll_to_form = document.getElementById('scroll-to-form');
-const form_position = document.getElementById('section-form').offsetTop - 60;
-scroll_to_form.addEventListener('click', function() {
-  scrollTo(document.body, form_position, 400);
-  scrollTo(document.documentElement, form_position, 400);
-});
+const form_position = document.getElementById('section-form').offsetTop - 58;
 
-function scrollTo(element, to, duration) {
+const scrollTo = (element, to, duration) => {
   if (duration <= 0) return;
-  var difference = to - element.scrollTop;
-  var perTick = difference / duration * 10;
+  let difference = to - element.scrollTop;
+  let perTick = difference / duration * 10;
 
-  setTimeout(function() {
+  setTimeout(() => {
     element.scrollTop = element.scrollTop + perTick;
     if (element.scrollTop === to) return;
     scrollTo(element, to, duration - 10);
   }, 10);
 }
+
+scroll_to_form.addEventListener('click', () => {
+  scrollTo(document.body, form_position, 400);
+  scrollTo(document.documentElement, form_position, 400);
+});
