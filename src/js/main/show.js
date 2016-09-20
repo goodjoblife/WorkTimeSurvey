fa78f1e65b6c3cfa601468d1ab19babc5d60b50e
Yen-chi Chen <mark86092@gmail.com>
2016-09-21 02:19:00 +0800

    vue, router, latest workings page
    * vue framework
    * director router
    * basic latest workings page

--- src/js/main/show.js
+++ src/js/main/show.js
@@ -1,53 +1,58 @@
-let lateset_url = 'https://tranquil-fortress-92731.herokuapp.com/workings/latest?page=0&limit=20';
-ajaxWrap(lateset_url)
-  .then(data => data.workings)
-  .then(workings => {
-    let item = workings.map(obj => {
-      appendLatest(obj.company.name, obj.job_title, obj.week_work_time, obj.overtime_frequency);
-    });
-    return workings;
-  })
-  .then(workings => {
-    let table_time_bar = document.querySelectorAll('.table-time-bar');
-    for (let i = 0; i < table_time_bar.length; i++) {
-      table_time_bar[i].style.width = table_time_bar[i].dataset.time * 2 + 'px';
+const latestWorkings = Vue.extend({
+  template: "#app-latest-workings",
+  data: function () {
+    return {
+      workings: [],
+    };
+  },
+  events: {
+    load_latest_workings: function() {
+      this.loadLatestWorkings();
+    },
+  },
+  methods: {
+    loadLatestWorkings: function() {
+      this.getLatestWorkings().then((res) => {
+        this.workings = res.data.workings;
+      }, (err) => {
+        // TODO
+      });
+    },
+    getLatestWorkings: function() {
+      const opt = {
+        params: {
+          page: 0,
+          limit: 20,
+        }
+      };
+      return this.$http.get(`${WTS.constants.backendURL}workings/latest`, opt);
     }
-  })
-  .catch(err => {
-    console.log(err);
-  });
+  },
+});
 
-const appendLatest = (company, job_title, week_work_time, overtime_frequency) => {
-  let to_be_append = `
-    <tr>
-      <td data-th="公司名稱">
-        <span class="rwd-td"><a href="#">${company}</a></span>
-      </td>
-      <td data-th="職稱 (廠區/門市/分公司)">
-        <span class="rwd-td"><span>${job_title}</span><span class="table-sector">ooxx</span></span>
-      </td>
-      <td data-th="一週總工時">
-        <span class="rwd-td"><span class="table-time-bar" data-time="${week_work_time}">${week_work_time}</span></span>
-      </td>
-      <td data-th="加班頻率">
-        <span class="rwd-td"><span class="table-frequency-circle" data-frequency="${overtime_frequency}"></span>${overtime_frequency}</span>
-      </td>
-    </tr>
-  `;
-  $('#tbody-latest-data').append(to_be_append);
-}
+const app = new Vue({
+  el: "#app",
+  components: {
+    'latestWorkings': latestWorkings,
+  },
+  data: {
+    currentView: null,
+  }
+});
 
-function ajaxWrap(reqPath) {
-  return new Promise((resolve, reject) => {
-    $.ajax({
-      type: 'GET',
-      url: reqPath,
-      success(data) {
-        resolve(data);
-      },
-      error(err) {
-        reject(err);
-      }
+const router = Router({
+  "/latest": function() {
+    app.currentView = "latestWorkings";
+    Vue.nextTick(function() {
+      app.$broadcast("load_latest_workings", 0);
+
+      //ga('send', 'event', category, 'visit-latest');
     });
-  });
-}
+  },
+}).configure({
+  notfound: function() {
+    router.setRoute("/latest");
+  }
+});
+
+router.init(["/"]);
