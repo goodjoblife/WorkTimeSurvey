0bd4e8ad65009bfbc703973cd7a81cadbd51a2bc
barry800414 <barry800414@users.noreply.github.com>
2017-01-15 23:01:00 +0800

    首頁 上傳表單：使用者推薦字串 (#389)
    * upload recommendation_string
    
    * change function
    
    * Add reference

--- src/js/main/index.js
+++ src/js/main/index.js
@@ -263,6 +263,19 @@ const checkFormField = () => {
   }
 };
 
+/*
+ * Get the recommendation string from url
+ *
+ * Adapted from http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+ */
+function getRecommendationString(url = window.location.href) {
+  const regex = new RegExp("[?&]rec_by(=([^&#]*)|&|#|$)");
+  const results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
 const sendFormData = () => {
   const data = domToData();
 
@@ -283,6 +296,11 @@ const sendFormData = () => {
   data.company = data.company_query;
   delete data.company_query;
 
+  //get recommendation_string if it exists
+  const rec_str = getRecommendationString();
+  if(rec_str !== null){
+    data.recommendation_string = rec_str;
+  }
 
   $.ajax({
     url: WTS.constants.backendURL + "workings",
