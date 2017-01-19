d54aef7e72724807d910e962ad55011ec05940d6
barry800414 <barry800414@users.noreply.github.com>
2017-01-19 14:15:24 +0800

    【查詢資訊頁面更新】單筆資料、以公司/查詢時的標題 更新 (#421)
    * single data page title
    
    * section title for searching by job title
    
    * section title for searching by company

--- src/js/main/show.js
+++ src/js/main/show.js
@@ -378,6 +378,43 @@ Vue.filter('formatted_wage_string', value => {
     return value;
 });
 
+Vue.filter('time_and_salary_section_title', value => {
+    const names = {
+      "created_at_descending": "最新薪時資訊",
+      "created_at_ascending": "最舊薪時資訊",
+      "week_work_time_descending": "工時排行榜",
+      "week_work_time_ascending": "工時排行榜（由低到高）",
+      "estimated_hourly_wage_descending": "估算時薪排行榜",
+      "estimated_hourly_wage_ascending": "估算時薪排行榜（由低到高）",
+    };
+
+    if(typeof value === 'object'){
+      if("sort_by" in value && "order" in value){
+        const key = `${value.sort_by}_${value.order}`;
+        return names[key];
+      }
+    }
+    return "最新薪時資訊";
+});
+
+Vue.filter('search_by_job_title_section_title', (value) => {
+    const names = {
+      "week_work_time_descending": "工時排行榜",
+      "week_work_time_ascending": "工時排行榜（由低到高）",
+      "estimated_hourly_wage_descending": "估算時薪排行榜",
+      "estimated_hourly_wage_ascending": "估算時薪排行榜（由低到高）",
+    };
+
+    if(typeof value === 'object'){
+      if("group_sort_by" in value && "order" in value){
+        const key = `${value.group_sort_by}_${value.order}`;
+        return names[key];
+      }
+    }
+    return "工時排行榜";
+});
+
+
 const app = new Vue({
   el: "#app",
   components: {
