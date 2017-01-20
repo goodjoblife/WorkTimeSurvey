cf89c6ea9d0f75ee75a3f025500bb1a55cd86fa6
Wei-Ming <barry800414@gmail.com>
2017-01-20 11:54:55 +0800

    check number string contain only [0-9] and '.' and follow mm[.nn] format

--- src/js/main/index.js
+++ src/js/main/index.js
@@ -214,6 +214,9 @@ const checkFormField = () => {
       if(isNaN(data.salary_amount)){
         throw new ValidationError("薪資並非數字", $("#salary-amount"));
       }
+      if(!isPureNumberString(data.salary_amount)){
+        throw new ValidationError('僅能包含0到9或小數點，不能有逗號或國字。', $("#salary-amount"));
+      }
 
       if (data.experience_in_year === "") {
         throw new ValidationError("需填寫相關職務工作經歷", $("#form-experience-in-year"));
@@ -223,6 +226,9 @@ const checkFormField = () => {
     if ($("#form-day-promised-work-time").hasClass('is-required')) {
       if (data.day_promised_work_time === "") {
         throw new ValidationError("需填寫工作日表定工作時間", $("#form-day-promised-work-time"));
+      }
+      if(!isPureNumberString(data.day_promised_work_time)){
+        throw new ValidationError('僅能包含0到9或小數點，不能有逗號或國字。', $("#form-day-promised-work-time"));
       }
       data.day_promised_work_time = parseFloat(data.day_promised_work_time);
       if (isNaN(data.day_promised_work_time)) {
@@ -235,6 +241,9 @@ const checkFormField = () => {
       if (data.day_real_work_time === "") {
         throw new ValidationError("需填寫工作日實際工作時間", $("#form-day-real-work-time"));
       }
+      if(!isPureNumberString(data.day_real_work_time)){
+        throw new ValidationError('僅能包含0到9或小數點，不能有逗號或國字。', $("#form-day-real-work-time"));
+      }
       data.day_real_work_time = parseFloat(data.day_real_work_time);
       if (isNaN(data.day_real_work_time)) {
         throw new ValidationError("工作日實際工作時間並非數字", $("#form-day-real-work-time"));
@@ -245,6 +254,9 @@ const checkFormField = () => {
 
       if (data.week_work_time === "") {
         throw new ValidationError("需填寫一週總工時", $("#form-week-work-time"));
+      }
+      if(!isPureNumberString(data.week_work_time)){
+        throw new ValidationError('僅能包含0到9或小數點，不能有逗號或國字。', $("#form-week-work-time"));
       }
       data.week_work_time = parseFloat(data.week_work_time);
       if (isNaN(data.week_work_time)) {
@@ -264,6 +276,13 @@ const checkFormField = () => {
     }
   }
 };
+
+//to check whether the number string contains only [0-9] and no more than one "." (of course, no comma)
+//and the format should be like mm[.nn]  m,n are [0-9] digit.
+function isPureNumberString(str){
+  const re = /^\d+(\.\d*)?$/;
+  return re.test(str);
+}
 
 /*
  * Get the recommendation string from url
