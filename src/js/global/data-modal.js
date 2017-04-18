6d9989094b49ac32fc34c8287c7973a548d1d6c6
Tin <wutingy@gmail.com>
2017-04-18 23:37:17 +0800

    fixed #468

--- src/js/global/data-modal.js
+++ src/js/global/data-modal.js
@@ -1,19 +1,21 @@
-$('body').on('click', '[data-modal-id]', function() {
-  $('body, html').addClass('is-fixed');
-  const modal_id = $(this).attr('data-modal-id');
-  const $modal = $(`[data-modal="${modal_id}"]`);
-  $modal.addClass('is-open');
-});
-const closeModal = () => {
-  $('.modal').removeClass('is-open');
-  $('body, html').removeClass('is-fixed');
-};
+$(function() {
+  $('[data-modal-id]').on('click', function() {
+    $('body, html').addClass('is-fixed');
+    const modal_id = this.dataset.modalId;
+    const $modal = $(`[data-modal="${modal_id}"]`);
+    $modal.addClass('is-open');
+  });
+  const closeModal = () => {
+    $('.modal').removeClass('is-open');
+    $('body, html').removeClass('is-fixed');
+  };
 
-$('body').on('click', '.js-close-data-modal', () => {
-  closeModal();
-});
-$('body').on('click', '.data-modal', (event) => {
-  if (!$(event.target).closest('.js-data-modal-container').length) {
+  $('body').on('click', '.js-close-data-modal', () => {
     closeModal();
-  }
+  });
+  $('body').on('click', '.data-modal', (event) => {
+    if (!$(event.target).closest('.js-data-modal-container').length) {
+      closeModal();
+    }
+  });
 });
