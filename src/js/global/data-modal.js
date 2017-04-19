9b8c045d941d752587a76635fab75fee66b38408
Tin <wutingy@gmail.com>
2017-04-19 21:40:11 +0800

    Fixed modal (#483)
    * fixed #468
    
    * #483 將 mobile 與 desktop 分開寫

--- src/js/global/data-modal.js
+++ src/js/global/data-modal.js
@@ -1,19 +1,28 @@
-$('body').on('click', '[data-modal-id]', function() {
-  $('body, html').addClass('is-fixed');
-  const modal_id = $(this).attr('data-modal-id');
-  const $modal = $(`[data-modal="${modal_id}"]`);
-  $modal.addClass('is-open');
-});
-const closeModal = () => {
-  $('.modal').removeClass('is-open');
-  $('body, html').removeClass('is-fixed');
-};
+$(function() {
+  $('body').on('click', '[data-modal-id]', function() {
+    $('body, html').addClass('is-fixed');
+    const modal_id = this.dataset.modalId;
+    const $modal = $(`[data-modal="${modal_id}"]`);
+    $modal.addClass('is-open');
+  });
 
-$('body').on('click', '.js-close-data-modal', () => {
-  closeModal();
-});
-$('body').on('click', '.data-modal', (event) => {
-  if (!$(event.target).closest('.js-data-modal-container').length) {
+  $('[data-modal-mobile-id]').on('click', function() {
+    const modal_id = this.dataset.modalMobileId;
+    const $modal = $(`[data-modal="${modal_id}"]`);
+    $modal.addClass('is-open');
+  });
+
+  const closeModal = () => {
+    $('.modal').removeClass('is-open');
+    $('body, html').removeClass('is-fixed');
+  };
+
+  $('body').on('click', '.js-close-data-modal', () => {
     closeModal();
-  }
+  });
+  $('body').on('click', '.data-modal', (event) => {
+    if (!$(event.target).closest('.js-data-modal-container').length) {
+      closeModal();
+    }
+  });
 });
