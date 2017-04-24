8bbdd989bab7f3ffa6e4f88fcef8d70a7367bdaf
Yen-chi Chen <mark86092@gmail.com>
2017-04-25 02:33:02 +0800

    Deploy setting

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,12 @@ cp CNAME _public/
 # patch for the facebook og image
 mkdir -p _public/img
 cp src/img/common/og-image_1200-630.png _public/img/
+
+mv _public/about.html _public/about
+mv _public/privacy-policy.html _public/privacy-policy
+mv _public/user-term.html _public/user-term
+mv _public/time-and-salary.html _public/time-and-salary
+
 cd _public
 git add -A .
 git commit -m "regen for $REV"
