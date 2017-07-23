9c0835131384def46c420899a1775f9727883ab3
Yen-chi Chen <mark86092@gmail.com>
2017-07-23 23:51:08 +0900

    Remove old static file (#501)

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -18,9 +18,6 @@ cp src/img/common/og-image_1200-630.png _public/img/
 
 mkdir -p _public/share
 mv _public/index.html _public/share/time-and-salary
-mv _public/about.html _public/about
-mv _public/privacy-policy.html _public/privacy-policy
-mv _public/user-term.html _public/user-term
 mv _public/time-and-salary.html _public/time-and-salary
 
 cd _public
